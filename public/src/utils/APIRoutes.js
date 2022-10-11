3a916ac7e1907c5ce6250e6224a4d2c85abe1370
Ruhulcse <ruhul.cse7862@gmail.com>
2022-10-10 21:34:48 -0500

    avatar section working

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,3 +1,4 @@
 const host = "http://localhost:5100";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
+export const setAvaterRoute = `${host}/api/auth/setAvatar`;
